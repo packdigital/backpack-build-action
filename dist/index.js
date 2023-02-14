5432db47af6c2e976d05719e4565eb0dba51ab96
Tyler Shambora <tyler@theshamboras.com>
2023-02-14 13:13:16 -0800

    rebuild

--- dist/index.js
+++ dist/index.js
@@ -76,21 +76,23 @@ function run() {
             if (packApiUrl) {
                 core.exportVariable('PACK_API_URL', packApiUrl);
             }
-            const resp = yield exec.exec(`sudo /usr/bin/netlify`, ['deploy', '--build', getDeployCommand(), '--message', getMessage()], {
-                env: {
-                    NETLIFY_SITE_ID: core.getInput('netlify_site_id'),
-                    BACKPACK_SITE_ID: core.getInput('backpack_site_id'),
-                    CMS_CONTENT_TOKEN: core.getInput('cms_content_token'),
-                    SHOPIFY_DOMAIN: core.getInput('shopify_domain'),
-                    SITE_URL: core.getInput('site_url'),
-                    NETLIFY_AUTH_TOKEN: core.getInput('netlify_auth_token'),
-                    BACKPACK_API_SECRET_TOKEN: core.getInput('backpack_api_secret_token'),
-                    CMS_MANAGEMENT_TOKEN: core.getInput('cms_management_token'),
-                    SHOPIFY_ADMIN_API_TOKEN: core.getInput('shopify_admin_api_token'),
-                    SHOPIFY_STOREFRONT_API_TOKEN: core.getInput('shopify_storefront_api_token')
-                }
-            });
-            console.log('resp', resp);
+            core.exportVariable('NETLIFY_SITE_ID', core.getInput('netlify_site_id'));
+            core.exportVariable('BACKPACK_SITE_ID', core.getInput('backpack_site_id'));
+            core.exportVariable('CMS_CONTENT_TOKEN', core.getInput('cms_content_token'));
+            core.exportVariable('SHOPIFY_DOMAIN', core.getInput('shopify_domain'));
+            core.exportVariable('SITE_URL', core.getInput('site_url'));
+            core.exportVariable('NETLIFY_AUTH_TOKEN', core.getInput('netlify_auth_token'));
+            core.exportVariable('BACKPACK_API_SECRET_TOKEN', core.getInput('backpack_api_secret_token'));
+            core.exportVariable('CMS_MANAGEMENT_TOKEN', core.getInput('cms_management_token'));
+            core.exportVariable('SHOPIFY_ADMIN_API_TOKEN', core.getInput('shopify_admin_api_token'));
+            core.exportVariable('SHOPIFY_STOREFRONT_API_TOKEN', core.getInput('shopify_storefront_api_token'));
+            yield exec.exec(`sudo /usr/bin/netlify`, [
+                'deploy',
+                '--build',
+                getDeployCommand(),
+                '--message',
+                getMessage()
+            ]);
         }
         catch (error) {
             if (error instanceof Error)
