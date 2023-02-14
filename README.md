3d2cbba3ce81df999bdada297a9f8b362985b97e
Tyler Shambora <tyler@theshamboras.com>
2023-02-14 05:41:10 -0800

    first pass

--- README.md
+++ README.md
@@ -103,3 +103,29 @@ See the [actions tab](https://github.com/actions/typescript-action/actions) for 
 ## Usage:
 
 After testing you can [create a v1 tag](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md) to reference the stable and latest V1 action
+
+---
+
+# Hello world javascript action
+
+This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+```yaml
+uses: actions/hello-world-javascript-action@v1.1
+with:
+  who-to-greet: 'Mona the Octocat'
+```
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,82 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+
+const getMessage = (): string => {
+  const messageParts = [`Run id: ${github.context.runId}`]
+
+  const branch: string = core.getInput('branch')
+
+  if (branch) {
+    messageParts.push(`Branch: ${branch}`)
+  }
+
+  const commitMessage: string | undefined =
+    github?.context?.payload?.head_commit?.message
+
+  if (commitMessage) {
+    messageParts.push(commitMessage)
+  }
+
+  if (!branch && !commitMessage) {
+    messageParts.push('Deploy to production')
+  }
+
+  return messageParts.join(' | ')
+}
+
+const getDeployCommand = (): string => {
+  const branch: string = core.getInput('branch')
+
+  if (branch) {
+    return `--alias ${branch}`
+  }
+
+  const autoDeployNetlifyDisabled: string = core.getInput(
+    'auto_deploy_netlify_disabled'
+  )
+
+  if (autoDeployNetlifyDisabled) {
+    return '--prod-if-unlocked'
+  }
+
+  return '--prod'
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const netlifyAuthToken: string = core.getInput('netlify_auth_token')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (!netlifyAuthToken) {
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const packApiUrl: string = core.getInput('pack_api_url')
+
+    if (packApiUrl) {
+      core.exportVariable('PACK_API_URL', packApiUrl)
+    }
+
+    const env = {
+      NETLIFY_SITE_ID: core.getInput('netlify_site_id'),
+      BACKPACK_SITE_ID: core.getInput('backpack_site_id'),
+      CMS_CONTENT_TOKEN: core.getInput('cms_content_token'),
+      SHOPIFY_DOMAIN: core.getInput('shopify_domain'),
+      SITE_URL: core.getInput('site_url'),
+      NETLIFY_AUTH_TOKEN: core.getInput('netlify_auth_token'),
+      BACKPACK_API_SECRET_TOKEN: core.getInput('backpack_api_secret_token'),
+      CMS_MANAGEMENT_TOKEN: core.getInput('cms_management_token'),
+      SHOPIFY_ADMIN_API_TOKEN: core.getInput('shopify_admin_api_token'),
+      SHOPIFY_STOREFRONT_API_TOKEN: core.getInput(
+        'shopify_storefront_api_token'
+      )
+    }
+
+    exec.exec(
+      `netlify`,
+      ['deploy', `--build ${getDeployCommand()}`, `--message ${getMessage()}`],
+      {env}
+    )
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
