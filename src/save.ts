ea2bb8701d05b208b3485af67cc4fd6680ee634e
Douglas Francisco <douglas.cardoso@packdigital.com>
2023-05-08 18:48:58 -0300

    Save cache just when exist netlify_auth_token

--- src/save.ts
+++ src/save.ts
@@ -1,9 +1,14 @@
+import * as core from '@actions/core'
 import {cachePaths, primaryKey} from './cache/constants'
 import {StateProvider} from './cache/state-provider'
 import saveImpl from './cache/save-impl'
 
 async function run(): Promise<void> {
-  await saveImpl(new StateProvider(), primaryKey, cachePaths)
+  const netlifyAuthToken: string = core.getInput('netlify_auth_token')
+
+  if (netlifyAuthToken) {
+    await saveImpl(new StateProvider(), primaryKey, cachePaths)
+  }
 }
 
 run()
